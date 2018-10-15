f95cb9f3cea71675d181463fdd1279ca1b29f204
wangxq <15065311065@163.com>
2018-10-15 20:23:53 +0800

    create count.py

---
+++ count.py
@@ -0,0 +1,10 @@
+#-*-coding: utf-8 -*-
+#Edited by sunrise_forever
+"""
+This is just a test file
+"""
+for i in range(100):
+    print(i**2)
+    if i > 90:
+        break
+print("The above is the number from 0 to 99")
